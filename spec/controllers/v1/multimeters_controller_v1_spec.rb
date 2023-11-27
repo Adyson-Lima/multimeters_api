489e6b0d24d69ea4233e1a06c6eacb6da6d4da20
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-27 16:20:29 -0300

    12° commit - tdd controller: Consegue listar um multimeter especifico e retornar status 200? GREEN

--- spec/controllers/v1/multimeters_controller_v1_spec.rb
+++ spec/controllers/v1/multimeters_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::MultimetersController, type: :controller do
     end
   end
 
+  describe "GET /api/v1/multimeters/id" do
+    it "Consegue listar um multimeter especifico e retornar status 200?" do
+      get :show, params: {id: @multimeter.id}
+      expect(response.body).to include_json(id: @multimeter.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
