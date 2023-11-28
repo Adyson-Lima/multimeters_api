3b3f5d91d4e9ccc3958cb57311e991ae61bc8255
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-28 15:14:19 -0300

    20° commit - seeds.rb configurado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Gerando multimeters...'
+
+5.times do |i|
+  Multimeter.create(
+    mult_type: ["analogico", "digital"].sample ,
+    description: ["tipo 1","tipo2"].sample
+    )
+end
+
+puts 'multimeters gerados com sucesso!'
